92313c0895741844d9d9062b6699edc1fa45019c
Atiar-Rahman <atiar1709@gmail.com>
2025-12-11 22:00:20 +0600

    custom user permission added

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    model = User
+    # Make sure you include only editable fields
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name","phone_number")}),
+        (_("Permissions"), {"fields": ("is_active","is_supervisor", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        (_("Important dates"), {"fields": ("last_login",)}),  
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "password1", "password2", "is_staff", "is_active"),
+        }),
+    )
+
+    list_display = ("email", "first_name", "last_name", "is_staff", "is_superuser", "is_active")
+    search_fields = ("email", "first_name", "last_name")
+    ordering = ("email",)
